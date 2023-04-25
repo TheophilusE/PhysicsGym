b402ec4becf0764c243a8cc8b9515d5be3d1ee12
Theophilus Eriata <theoeriata@gmail.com>
2023-04-24 18:04:30 -0600

    Add Object Spawning Through Script.

--- Content/Scripts/FrameObjectCounter.ts
+++ Content/Scripts/FrameObjectCounter.ts
@@ -31,6 +31,35 @@ export class FrameObjectCounter extends xii.TickedTypescriptComponent
   Tick(): void
   {
     ++this.iCounter;
+
+    let owner = this.GetOwner();
+    
+    let objectDesc : xii.GameObjectDesc = new xii.GameObjectDesc();
+    {
+      objectDesc.Name = "Object" + this.iCounter;
+      objectDesc.ActiveFlag = true;
+      objectDesc.Dynamic = true;
+      objectDesc.LocalPosition = owner.GetGlobalPosition();
+      // objectDesc.LocalRotation = new xii.Quat(0.0, 0.0, 0.0);
+      // objectDesc.LocalScaling = new xii.Vec3(0.0, 0.0, 0.0);
+    }
+
+    let spawnedObject : xii.GameObject =  xii.World.CreateObject(objectDesc);
+    if (spawnedObject == null)
+    {
+      xii.Log.Warning("Failed to create game object for physics body '" + this.iCounter + "'");
+      return;
+    }
+
+    let prefabReferenceComponent : xii.PrefabReferenceComponent = xii.World.CreateComponent(spawnedObject, xii.PrefabReferenceComponent);
+    if (prefabReferenceComponent == null)
+    {
+      xii.Log.Warning("Failed to create prefab reference component for physics ")
+      return;
+    }
+    
+    prefabReferenceComponent.Prefab = "{ ca4ffce1-3d7e-4186-a7aa-971b44472e9a }";
+    
     xii.Debug.DrawInfoText(xii.Debug.ScreenPlacement.TopCenter, "Physics Body Count: " + this.iCounter, xii.Color.GreenYellow());
   }
 }
